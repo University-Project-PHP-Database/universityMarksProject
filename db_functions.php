bb0b770d68a41f41f3482261ad5054d9cb266649
Hiba Fneish <153556261+hibafn97@users.noreply.github.com>
2024-01-05 21:35:32 +0200

    edit in dbFunctions

--- db_functions.php
+++ db_functions.php
@@ -20,4 +20,44 @@
         
     }
     
+    // insert a course to the database, $info is an array contain all the information of the element 
+    function insert($tableName, $Info){
+        $connect = database_connection();
+        foreach ($Info as $coloumnName => $coloumnValue) {          
+            $set_info = "INSERT INTO $tableName ($coloumnName) VALUES ('$coloumnValue') ";
+        }
+        $result = mysqli_query($connect, $set_info) or die("Query failed: " . mysqli_error($connect));
+        
+        mysqli_close($connect);
+        
+    }
+
+    function delete($tableName, $eid){
+
+        $connect = database_connection();
+        $query = "delete from '$tableName' where product_id='$eid'";
+        $result = mysqli_query($connect, $query) or die("Query failed: " . mysqli_error($connect));
+        
+        mysqli_close($connect);
+    }
+
+    function search($tableName, $id, $eid){
+        $connect = database_connection();
+        $query = "SELECT * from '$tableName' where '".$id."'='".$eid."'"; 
+        $result = mysqli_query($connect, $query) or die("Query failed: " . mysqli_error($connect));
+
+        mysqli_close($connect);
+
+        return $result;
+    }
+
+    function edit($tableName,$Info,$id,$eid){
+        $connect = database_connection();
+        foreach ($Info as $coloumnName => $coloumnValue) {          
+            $set_info = "update '$tableName' set '$coloumnName' = '$coloumnValue' where '$id' = '$eid' ";
+        }
+        $result = mysqli_query($connect, $set_info) or die("Query failed: " . mysqli_error($connect));
+        
+        mysqli_close($connect);
+    }
 ?>
