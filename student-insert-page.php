d28829290af2712c8b744a7feab21251886b738f
Hiba Fneish <153556261+hibafn97@users.noreply.github.com>
2024-01-18 21:44:35 +0200

    my message here

--- student-insert-page.php
+++ student-insert-page.php
@@ -2,17 +2,22 @@
 include("db_functions.php");
 $con = database_connection();
 $status = "";
-if(isset($_POST['sid']) && isset($_POST['name']) && isset($_POST['dateofbirth']) && isset($_POST['address']) && isset($_POST['phone'])){
+if(isset($_POST['sid']) && isset($_POST['name']) && isset($_POST['dateofbirth']) && isset($_POST['address']) && isset($_POST['phone']) && isset($_POST['email']) && isset($_POST['password'])){
     $id = $_POST['sid'];
     $name =$_POST['name'];
     $trn_date = date("Y-m-d ",strtotime($_POST['dateofbirth']));
     $address = $_POST['address'];
     $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
     $ins_query="insert into student
     (`sid`,`sname`,`bdate`,`address`,`phone`)values
     ('$id','$name','$trn_date','$address','$phone')";
     mysqli_query($con,$ins_query)
     or die(mysqli_error($con));
+    $ins_query2="insert into loginstudents (`student`, `email`, `password`) values
+     ('$id', '$email', '$password')";
+     mysqli_query($con,$ins_query2) or die(mysqli_error($con));
     $status = "New Record Inserted Successfully.
     </br></br><a href='student-view-page.php'>View Inserted student</a>";
     mysqli_close($con);
@@ -41,6 +46,8 @@ if(isset($_POST['sid']) && isset($_POST['name']) && isset($_POST['dateofbirth'])
                 </div>
                 <p><input type="text" name="address" placeholder="Enter Address" required /></p>
                 <p><input type="text" name="phone" placeholder="Enter Phone Number" required /></p>
+                <p><input type="text" name="email" placeholder="Enter Email" required /></p>
+                <p><input type="text" name="password" placeholder="Enter Password" required /></p>
                 <p><input name="submit" type="submit" value="Submit" /></p>
                 </form>
                 <p style="color:#FF0000;"><?php echo $status; ?></p>
