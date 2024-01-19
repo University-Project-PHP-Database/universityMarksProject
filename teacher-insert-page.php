3580a61e48480c603b4b5dcae5327827adac94ef
Hiba Fneish <153556261+hibafn97@users.noreply.github.com>
2024-01-19 11:44:37 +0200

    my message here

--- teacher-insert-page.php
+++ teacher-insert-page.php
@@ -2,16 +2,21 @@
 include("db_functions.php");
 $con = database_connection();
 $status = "";
-if(isset($_POST['tid']) && isset($_POST['tname'])  && isset($_POST['address']) && isset($_POST['phone'])&& isset($_POST['speciality'])){
+if(isset($_POST['tid']) && isset($_POST['tname'])  && isset($_POST['address']) && isset($_POST['phone'])&& isset($_POST['speciality']) && isset($_POST['email']) && isset($_POST['password'])){
     $id = $_POST['tid'];
     $name =$_POST['tname'];
     $address = $_POST['address'];
     $phone = $_POST['phone'];
     $speciality = $_POST['speciality'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $type = $_POST['type'];
     $ins_query="insert into teacher (`tid`,`tname`,`address`,`phone`, `speciality`) values
     ('$id','$name','$address','$phone','$speciality')";
     mysqli_query($con,$ins_query)
     or die(mysqli_error($con));
+    $ins_query2="insert into loginstudents (`doctor`, `email`, `password`,`type`) values
+    ('$id', '$email', '$password','$type')";
     $status = "New Teacher Inserted Successfully.
     </br></br><a href='teacher-view-page.php'>View Inserted teacher</a>";
     mysqli_close($con);
@@ -37,6 +42,9 @@ if(isset($_POST['tid']) && isset($_POST['tname'])  && isset($_POST['address']) &
                 <p><input type="text" name="address" placeholder="Enter Address" required /></p>
                 <p><input type="text" name="phone" placeholder="Enter Phone Number" required /></p>
                 <p><input type="text" name="speciality" placeholder="Enter Speciality" required /></p>
+                <p><input type="text" name="email" placeholder="Enter Email" required /></p>
+                <p><input type="text" name="password" placeholder="Enter Password" required /></p>
+                <p><input type="text" name="type" placeholder="Enter Type(A - D)" required /></p>
                 <p><input name="submit" type="submit" value="Submit" /></p>
                 </form>
                 <p style="color:#FF0000;"><?php echo $status; ?></p>
